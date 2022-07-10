32886c93f7b55a5f11b8cc9357d2abb09d42895f
OfekBar <75580980+OfekBar@users.noreply.github.com>
2022-07-10 11:38:24 +0300

    Add files via upload

---
+++ SRC/APPLICATION-SOURCE-CODE/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, url_for, request, json
+import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template('home_page.html')
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
